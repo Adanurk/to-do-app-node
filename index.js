b8f7e9707dde8bda816805816e75f40707db6799
Adanurk <gumussoykbr@gmail.com>
2022-05-16 11:34:01 +0200

    last updates

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ let commandList = [
   { id: 2, text: "delete any TODO" },
   { id: 3, text: "update any TODO" },
   { id: 4, text: "show pending TODO" },
-  { id: 4, text: "show done TODO" },
+  { id: 5, text: "show done TODO" },
+  {id:6, text:"add new TODO"}
 ];
 
 //printing command list
@@ -39,25 +40,50 @@ switch (input) {
     }
     break;
     case 2: //delete any todo
-    let deleteNum = +prompt("Enter the number of task you want to delete: ");
+    let deleteNum =  +prompt("Enter the number of task you want to delete: ");
     let ourTask;
     for (const item of todoList) {
         if(item["id"]===deleteNum){
             ourTask = item;
         }
     }
-    // console.log(ourTask); //it gives the object with that id
-    let function1 = function(num){
-        return todoList.filter()
+
+    console.log(ourTask);
+
+
+    let removeTodo = function(){
 
     }
 
     break;
-  case 3: //update any todo
+  case 3: //Update items as done
+
+  let editTodo = function(){
+
+  }
+
     break;
-  case 4: //show pending todos
+  case 4: //show pending todos //console log the tasks with done property false 
+
+  let pendingTodo  =function(){
+
+  }
+
     break;
-  case 5: //show done todos
+  case 5: //show done todos //done property true
+
+  let doneTodos = function(){
+
+  }
+
+    break;
+    case 6: //adding new TODO item //Don’t allow adding empty items
+
+    let insertTodo = function(){
+
+    }
+
+
     break;
   default:
     console.log("Enter a command number");
